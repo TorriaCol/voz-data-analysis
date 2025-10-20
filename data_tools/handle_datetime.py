bea821e665ed33e017ffb61bd302a246aaba5625
Torria Colpitts <torriacolpitts@gmail.com>
2025-10-20 14:51:16 -0600

    Added plantower calibration code and somewhat cleaned

--- data_tools/handle_datetime.py
+++ data_tools/handle_datetime.py
@@ -4,15 +4,14 @@ import pandas as pd
 #   Data has been resampled to hourly
 #   date_time is in UTC
 def create_utc_for_voz(data):
-    data['date_time'] = pd.to_datetime(data['date_time'], utc=True)
+    data['date_time'] = pd.to_datetime(data['date_time'])
     data.set_index('date_time',inplace=True)
     return data
 
 # Works if data is in PST (as of now, all data downloaded from CARB is PST)
 def create_utc_for_CARB(data):
     data['date_time'] = pd.to_datetime(
-        data['date'].astype(str) + ' ' + data['start_hour'].astype(str) + ':30',
-        utc=True
+        data['date'].astype(str) + ' ' + data['start_hour'].astype(str) + ':30'
     )
     data['date_time'] = data['date_time'] + pd.Timedelta(hours=8)
     data.set_index('date_time', inplace=True)
@@ -22,5 +21,10 @@ def utc_to_CA(data):
     data.index = pd.to_datetime(data.index, utc=True)
     data.index = data.index.tz_convert('US/Pacific')
     data.index = data.index.tz_localize(None)
+    return data
+
+def create_standard_datetime(data):
+    data['date_time'] = pd.to_datetime(data['date_time'])
+    data.set_index('date_time', inplace=True)
     return data
     
---
+++ data_tools/process_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+from . import handle_datetime
+def ref_data(path):
+    ref_data = pd.read_csv(path)
+    ref_data = handle_datetime.create_utc_for_CARB(ref_data)
+    ref_data['reference'] = ref_data['value']
+    return ref_data
+
+def raw_voz_data(path):
+    voz_data = pd.read_csv(path)
+    voz_data = handle_datetime.create_utc_for_voz(voz_data)
+    return voz_data
+
+def standard_data(path):
+    data = pd.read_csv(path)
+    data = handle_datetime.create_standard_datetime(data)
+    return data
