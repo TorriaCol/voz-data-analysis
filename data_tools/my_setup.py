2ce8b481f4aa876fd4e6c5127c210c244cd0866f
Torria Colpitts <torriacolpitts@gmail.com>
2025-10-26 22:58:18 -0600

    Calibration split up into classes and works with both sensors

--- data_tools/my_setup.py
+++ data_tools/my_setup.py
@@ -1,3 +1,3 @@
 ### Change mypath to return whatever relative path your data is in. This is the only place you will need to define it.
 def deployment_2023_path():
-    return r"../reference_files/2023rawdata/"
+    return r"../reference_files/WasteData/"
