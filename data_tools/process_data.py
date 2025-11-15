8ef597be5b1d68f1ca903adc9a1381fc22f853ed
Torria Colpitts <torriacolpitts@gmail.com>
2025-11-15 14:47:47 -0700

    Validated the aqlite and fixed some other stuff

--- data_tools/process_data.py
+++ data_tools/process_data.py
@@ -13,6 +13,13 @@ def raw_voz_data(path):
     voz_data = handle_datetime.create_utc_for_voz(voz_data)
     return voz_data
 
+def aqlite_data(path):
+    data = pd.read_csv(path)
+    data = data.rename(columns={'Ozone-1578:OZONE_date': 'date_time', 'Ozone-1578:OZONE_value': 'o3'})
+    data = data[['date_time', 'o3']]
+    data = handle_datetime.create_standard_datetime(data)
+    return data
+
 def standard_data(path):
     data = pd.read_csv(path)
     data = handle_datetime.create_standard_datetime(data)
